814e35577b8187a6803df94238172aae23ea8ff4
marmik <marmik@umich.edu>
2018-12-22 03:38:38 -0500

    Clean up file output

--- binary-index-tree/TestBinarySearchTree.py
+++ binary-index-tree/TestBinarySearchTree.py
@@ -1,3 +1,5 @@
+"""Test with random numbers"""
+
 import BinaryIndexTree
 import NaiveSolution
 import random
@@ -6,7 +8,7 @@ import sys
 array_size = 4
 max_elem = 10
 test_iterations = 2
-verbose = False
+verbose = True
 
 
 def main():
@@ -14,7 +16,10 @@ def main():
     if len(sys.argv) > 2:
         print("Invalid command line arguments")
     elif len(sys.argv) == 2:
-        verbose = (sys.argv[1] == "-v")
+        # No error checking
+        if sys.argv[1] == "-q":
+            global verbose
+            verbose = False
 
     for i in range(0, test_iterations):
         run_test()
@@ -22,17 +27,20 @@ def main():
 
 def test_sum(BIT, naive):
     sum_idx = random.randint(0, array_size - 1)
-    print("Function call: Sum from 0 to " + str(sum_idx))
+    function_call = "Function call: Sum from index 0 to index " + str(sum_idx)
 
     naive_add = naive.sum_to_index(sum_idx)
     BIT_add = BIT.sum_to_index(sum_idx)
+
+    return evaluate("BIT", naive_add,
+                    "Naive", naive_add, function_call)
 
 
 def test_update(BIT, naive):
     update_idx = random.randint(0, array_size - 1)
     update_value = random.randint(-max_elem, max_elem)
-    print("Function call: update " + str(update_idx) +
-          " by adding " + str(update_value))
+    function_call = "Function call: update index " + \
+        str(update_idx) + " by adding " + str(update_value)
 
     naive.update_value(update_idx, update_value)
     BIT.update_value(update_idx, update_value)
@@ -40,32 +48,49 @@ def test_update(BIT, naive):
     naive_idx_value = naive.sum_range(update_idx, update_idx)
     BIT_idx_value = BIT.sum_range(update_idx, update_idx)
 
+    return evaluate("BIT", BIT_idx_value,
+                    "Naive", naive_idx_value, function_call)
 
-def evaluate(pair1, pair2, function_call):
-    label1, value1 = pair1
-    label2, value2 = pair2
 
-    if value1 != value2:
-        print("INCONSISTENCY")
-        print(label1, value1)
-        print(label2, value2)
+def evaluate(label1, value1, label2, value2, function_call):
+    test_result = "TEST FAILED\n"
+    result = False
+
+    if value1 == value2:
+        test_result = "TEST PASSED\n"
+        result = True
+
+    output = ""
+    output += test_result + function_call
+
+    if verbose:
+        output += '\n' \
+            + label1 + " " + str(value1) + '\n' \
+            + label2 + " " + str(value2) \
+
+    print(output)
+    return result
 
 
 def run_test():
     array = []
     for j in range(0, array_size):
         array.append(random.randint(1, max_elem))
-
     print("Array: ", array)
 
     BIT = BinaryIndexTree.BinaryIndexTree(array)
     naive = NaiveSolution.NaiveSolution(array)
 
-    test_sum(BIT, naive)
+    sum1_result = test_sum(BIT, naive)
+    update_result = test_update(BIT, naive)
+    sum2_result = test_sum(BIT, naive)
 
-    test_update(BIT, naive)
+    all_tests_passed = sum1_result & sum2_result & update_result
+    if all_tests_passed:
+        print("Passed all tests this round")
 
-    test_sum(BIT, naive)
+    print('\n')
+    return
 
 
 if __name__ == "__main__":

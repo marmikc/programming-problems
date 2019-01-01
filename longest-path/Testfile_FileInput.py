1a87db764911e1c7309bd4052b87ccec8087d400
marmik <marmik@umich.edu>
2019-01-01 13:22:08 -0500

    Add correct answers in hardcoded test cases file

--- longest-path/Testfile_FileInput.py
+++ longest-path/Testfile_FileInput.py
@@ -7,6 +7,7 @@ test1 = [
     [2, 2, 2],
     [2, 2, 2]
 ]
+# 0 longest path
 
 # One path
 test2 = [
@@ -14,6 +15,7 @@ test2 = [
     [8, 1, 4],
     [7, 6, 5]
 ]
+# 8 longest path
 
 # One path
 test3 = [
@@ -21,6 +23,7 @@ test3 = [
         [4, 1, 8],
         [5, 6, 7]
 ]
+# 8 longest path
 
 # 2 merging paths
 test4 = [
@@ -28,6 +31,7 @@ test4 = [
     [3, 4, 5],
     [2, 9, 9]
 ]
+# 4 longest path
 
 # Same path lengths
 test5 = [
@@ -35,6 +39,7 @@ test5 = [
     [2, 9, 2],
     [2, 2, 2]
 ]
+# 1 longest path
 
 # One max, multiple minima
 test6 = [
@@ -45,6 +50,7 @@ test6 = [
         [0, 0, 0, 5, 0, 0],
         [1, 2, 3, 4, 3, 2]
 ]
+# 8 longest path
 
 # One minima, multiple maxima
 test7 = [
@@ -55,6 +61,7 @@ test7 = [
         [9, 9, 9, 5, 9, 9],
         [9, 8, 7, 6, 8, 9]
 ]
+# 8 longest path
 
 grid = test3
 for row in grid:

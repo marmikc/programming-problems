c140874267df9f2c133983d4752e7fe58c2462d9
marmik <marmik@umich.edu>
2019-01-03 14:22:45 -0500

    Add code to do frequency analysis of the two solutions with 100000 iterations

--- random-weighted-shuffle/FrequencyAnalysis.py
+++ random-weighted-shuffle/FrequencyAnalysis.py
@@ -3,16 +3,50 @@
 import NaiveSolution as NS
 import RandomWeightedShuffle as RWS
 
-NUM_ITERATIONS = 10000
+NUM_ITERATIONS = 100000
 
-testcase1 = [
-    ("Apple", 2),
-    ("Grape", 1),
-    ("Pea", 1),
-]
 
-naive = NS.WeightedShuffler(testcase1)
-print(naive.shuffle())
+def main():
+    testcase1 = [
+        ("Apple", 2),
+        ("Grape", 1),
+        ("Pea", 1),
+    ]
 
-solution = RWS.WeightedShuffler(testcase1)
-print(solution.shuffle())
+    naive = NS.WeightedShuffler(testcase1)
+    # print(naive.shuffle())
+
+    rws = RWS.WeightedShuffler(testcase1)
+    # print(rws.shuffle())
+
+    print("Performaing analysis on NAIVE SOLUTION")
+    perform_frequency_analysis(naive)
+
+    print("Performing analysis on BINARY TREE SOLUTION")
+    perform_frequency_analysis(rws)
+
+
+def perform_frequency_analysis(shuffler):
+    list_permutation_counts = {}
+    for count in range(0, NUM_ITERATIONS + 1):
+        shuffled = shuffler.shuffle()
+
+        preceeding_items = []
+        for item in shuffled:
+            if tuple(preceeding_items) not in list_permutation_counts:
+                list_permutation_counts[tuple(preceeding_items)] = 0
+            else:
+                list_permutation_counts[tuple(preceeding_items)] += 1
+
+            preceeding_items.append(item)
+
+    for key in list_permutation_counts:
+        # Probabilty of appearance
+        if key == ():
+            continue
+
+        prob = list_permutation_counts[key] / list_permutation_counts[key[:-1]]
+        print("Given: ", key[:-1], " | ", key[-1], " appears with prob ", prob)
+
+if __name__ == "__main__":
+    main()

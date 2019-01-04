1423034210e3fa569d55b04492a645741d5a1caa
marmikc <31773220+marmikc@users.noreply.github.com>
2019-01-04 16:34:47 -0500

    Rewrite comments for legibility

--- longest-path/Testfile_HardcodedInput.py
+++ longest-path/Testfile_HardcodedInput.py
@@ -7,7 +7,7 @@ test1 = [
     [2, 2, 2],
     [2, 2, 2]
 ]
-# 0 longest path
+# longest path length 0
 
 # One path
 test2 = [
@@ -15,7 +15,7 @@ test2 = [
     [8, 1, 4],
     [7, 6, 5]
 ]
-# 8 longest path
+# longest path length 8
 
 # One path
 test3 = [
@@ -23,7 +23,7 @@ test3 = [
         [4, 1, 8],
         [5, 6, 7]
 ]
-# 8 longest path
+# longest path length 8
 
 # 2 merging paths
 test4 = [
@@ -31,7 +31,7 @@ test4 = [
     [3, 4, 5],
     [2, 9, 9]
 ]
-# 4 longest path
+# longest path length 4
 
 # Same path lengths
 test5 = [
@@ -39,7 +39,7 @@ test5 = [
     [2, 9, 2],
     [2, 2, 2]
 ]
-# 1 longest path
+# longest path length 1
 
 # One max, multiple minima
 test6 = [
@@ -50,7 +50,7 @@ test6 = [
         [0, 0, 0, 5, 0, 0],
         [1, 2, 3, 4, 3, 2]
 ]
-# 8 longest path
+# longest path length 8
 
 # One minima, multiple maxima
 test7 = [
@@ -61,7 +61,7 @@ test7 = [
         [9, 9, 9, 5, 9, 9],
         [9, 8, 7, 6, 8, 9]
 ]
-# 8 longest path
+# longest path 8
 
 grid = test3
 for row in grid:
